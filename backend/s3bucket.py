c217c2cb418664d43155a62b859cc29848946764
Vetlets05 <vetlets@icloud.com>
2025-09-23 19:23:53 +0200

    style: format s3_bucket

--- backend/s3bucket.py
+++ backend/s3bucket.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-                       
+
 from pillow import Image
 
 from io import BytesIO
@@ -15,41 +15,30 @@ load_dotenv()
 # Retrieve AWS credentials
 
 aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
-
 aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-
 aws_bucket_name = os.getenv("AWS_BUCKET_NAME")
-
 aws_region = os.getenv("AWS_REGION")
 
-def convert_image_to_binary(image_path):
 
+def convert_image_to_binary(image_path):
     # Open the image and convert to binary
 
     with Image.open(image_path) as img:
-
         img_byte_array = BytesIO()
-
         img.save(img_byte_array, format=img.format)
-
         img_byte_array.seek(0)
 
     return img_byte_array
 
-def upload_image_to_s3(image_path, s3_key):
 
+def upload_image_to_s3(image_path, s3_key):
     # Initialize the S3 client with credentials
 
     s3_client = boto3.client(
-
         "s3",
-
         aws_access_key_id=aws_access_key,
-
         aws_secret_access_key=aws_secret_key,
-
-        region_name=aws_region
-
+        region_name=aws_region,
     )
 
     # Convert image to binary data
@@ -59,18 +48,7 @@ def upload_image_to_s3(image_path, s3_key):
     # Upload to S3
 
     try:
-        s3_client.upload_fileobj(
-
-            image_data,
-
-            aws_bucket_name,
-
-            s3_key
-
-        )
-
-        print(f”Image successfully uploaded to S3 at {aws_bucket_name}/{s3_key}”)
-
+        s3_client.upload_fileobj(image_data, aws_bucket_name, s3_key)
+        print(f"Image successfully uploaded to S3 at {aws_bucket_name}/{s3_key}")
     except Exception as e:
-
-        print(f”Error uploading image: {e}”)
+        print(f"Error uploading image: {e}")
