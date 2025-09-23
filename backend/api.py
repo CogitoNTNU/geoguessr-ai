f855bb61e81b7b0c79fd7a495e162369932a517e
Håkon Støren <hakonts@icloud.com>
2025-09-23 18:59:10 +0200

    feat: add some basic api endpoints

--- backend/api.py
+++ backend/api.py
@@ -1,41 +1,73 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
+
+import time
 
 app = FastAPI()
 
 
-class Item(BaseModel):
+class Image(BaseModel):
     name: str
-    price: float
-    is_offer: Union[bool, None] = None
+    id: int
+    description: str
+    file_path: str
 
 
-@app.get("/")
+class Model(BaseModel):
+    id: int
+    file_path: str
+
+
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"Hello": "World"}
+    return """<html>
+    <head>
+        <title>Some HTML in here</title>
+    </head>
+    <body>
+        <h1>Geoguessr AI API op side</h1>
+        <p>This is a simple HTML response from FastAPI.</p>
+    </body>
+    </html>"""
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/model/{model_id}")
+def read_model(model_id: int, q: Union[str, None] = None):
+    return {"model_id": model_id, "model_file_path": "1"}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/image/{image_id}")
+def read_image(image_id: int, q: Union[str, None] = None):
+    im = Image()
+    return {
+        "image_id": image_id,
+        "image_name": im.name,
+        "image_filepath": im.file_path,
+        "image_desc": im.description,
+    }
 
 
-########################################
+@app.post("/submit_image/")
+def submit_image():
+    return {"message": "Image has been added"}
+
+
+@app.get("/predicition/{image_id}")
+def get_prediction(image_id: int):
+    long = 1
+    lat = 2
+    return {"long": long, "lat": lat}
+
+
+@app.get("/health")
 def ping():
-    # fastapi.connect
-    # # /health
-    pass
+    return {"status": "Healthy!", "Timestamp": time.time()}
 
 
-def fastapi():
-    # last opp bilde
-    # hent modell fra fastapi
+if __name__ == "__main__":
+    import uvicorn
 
-    pass
+    uvicorn.run(app, host="0.0.0.0", port=7200)
