c6a7a46d4f4e833a56a5d8076fa2e4e51a0ff289
Håkon Støren <hakonts@icloud.com>
2025-09-27 17:54:42 +0200

    feat: add neighbour list to cell class

--- data/geocells/cell.py
+++ data/geocells/cell.py
@@ -1,6 +1,8 @@
 import shapely.ops
 import numpy as np
 from typing import List
+import pandas as pd
+import geopandas as gpd
 
 
 class Cell:
@@ -14,6 +16,18 @@ class Cell:
 
         self.country = country
         self.admin_1 = admin_1
+
+        self.neigbours = []
+
+    def add_point(self, point):
+        self.points.append(point)
+
+    def get_neighbours(self, geocells):
+        for cell in geocells:
+            if cell == self:
+                continue
+            if cell.shape().intersects(self.shape()):
+                self.neigbours.append(geocells.id)
 
     def shape(self):
         union = shapely.ops.unary_union(self.polygons)
@@ -30,20 +44,39 @@ class Cell:
         for other in others:
             self.points += other.points
             self.polygons += other.polygons
+            self.neigbours += other.neighbours
             other.points = []
-            other.points
+            other.polygons = []
+
+            for n in other.neighbours:
+                n.neighbours.remove(other.id)
+                n.append(self.id)
+
+            other.neighbours = []
+
+            self.neigbours.remove(other.id)
+            self.neigbours.remove(self.id)
 
     def split(self):
         pass
 
-    def substract(self, other):
+    def subtract(self, other):
         pass
 
-    def merge(self, other):
-        pass
+    def contains(self, point):
+        return self.shape().contains(point)
 
     def cluster(self):
         pass
+
+    def to_pandas(self):
+        data = [[self.id, p.x, p.y] for p in self.points]
+        columns = ["id", "lng", "lat"]
+        df = pd.DataFrame(data=data, columns=columns)
+        df = gpd.GeoDataFrame(
+            df, geometry=gpd.points_from_xy(df.lng, df.lat), crs="“EPSG:4326”"
+        )
+        return df
 
     def __eq__(self, other):
         return self.id == other.id
@@ -59,3 +92,11 @@ class Cell:
 
     def __str__(self):
         return self.__repr__()
+
+    def __hash__(self):
+        return hash(self.id)
+
+
+cell = Cell("Hallo", [], [], "Norway", "Rogaland")
+print(hash(cell))
+print(cell.is_empty())
