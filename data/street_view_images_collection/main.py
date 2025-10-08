99f3cbb0d138b2a2ec907ba7d270ed1d6092b169
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2025-10-09 00:16:47 +0200

    feat: make more of the script and it has some manual tests

--- data/street_view_images_collection/main.py
+++ data/street_view_images_collection/main.py
@@ -3,17 +3,17 @@ from google_api.street_view import sign_url
 import requests
 from urllib.parse import urlencode
 import random
-import math
+from dotenv import load_dotenv
 
-
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "../../.env"))
 GOOGLE_KEY = os.getenv("GOOGLE_MAPS_KEY")  # required
 SIGNING_SECRET = os.getenv("GOOGLE_MAPS_SECRET")
 IMAGE_BASE = "https://maps.googleapis.com/maps/api/streetview"
 
 
 def collect_google_streetview(lat: float, lon: float):
-    # Generate four random headings that cover 360 degrees
-    headings = [math.ceil(random.uniform(i * 90, (i + 1) * 90)) for i in range(4)]
+    seed = round(random.uniform(0, 90))
+    headings = [seed, seed + 90, seed + 180, seed + 270]
 
     for heading in headings:
         params = {
@@ -27,14 +27,22 @@ def collect_google_streetview(lat: float, lon: float):
         }
         url = f"{IMAGE_BASE}?{urlencode(params)}"
         url = sign_url(url, SIGNING_SECRET) if SIGNING_SECRET else url
+
         r = requests.get(url, timeout=30)
         if r.status_code != 200:
             raise RuntimeError(
                 f"Image fetch failed ({r.status_code}) for heading {heading}, lat {lat}, lon {lon}"
             )
-        with open("./out/" + heading, "wb") as f:
+
+        # Save image to file tmp
+        os.makedirs("out", exist_ok=True)
+        filename = f"out/streetview_{lat}_{lon}_heading_{heading}.jpg"
+        with open(filename, "wb") as f:
             f.write(r.content)
-        print(r)
+        print(f"Saved image to {filename}")
+
+        # Save image to s3 bucket
+        # TODO
 
 
 if __name__ == "__main__":
@@ -46,5 +54,9 @@ if __name__ == "__main__":
     )
     if extra_credits_result.lower() == "y":
         amount_of_pictures = 51_000
+
+    # TODO: Get the list of coordinates that is to be collected, and check if they have been collected already
+
+    # TODO: Make a new file that has the coordinates that has been collected
 
     collect_google_streetview(60.15805, 19.905592)
