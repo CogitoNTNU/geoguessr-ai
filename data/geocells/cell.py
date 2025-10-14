fb30f7663c3dd67995f7f7cc849f5a6ee521aef6
Håkon Støren <hakonts@icloud.com>
2025-10-14 17:22:42 +0200

    feat: add cell splitting (but it has errors)

--- data/geocells/cell.py
+++ data/geocells/cell.py
@@ -10,8 +10,10 @@ from loguru import logger
 from shapely.errors import TopologicalError
 from scipy.spatial import Voronoi
 from voronoi_polygon_2d import voronoi_finite_polygons_2d
+from sklearn.cluster import OPTICS
 
 CRS = "EPSG:4326"
+GEOCELL_COLUMNS = ["name", "admin_1", "country", "size", "num_polygons", "geometry"]
 
 
 class Cell:
@@ -32,6 +34,10 @@ class Cell:
 
     def add_point(self, point):
         self.points.append(point)
+        try:
+            self.curr_coords = self.coords()
+        except Exception:
+            print(f"{point} HLLLL")
 
     def get_neighbours(self, geocells):
         for cell in geocells:
@@ -46,7 +52,9 @@ class Cell:
         return union
 
     def coords(self):
-        return np.ndarray([(p.x, p.y) for p in self.points])
+        if len(self.points) > 0:
+            print(self.points[0], self.points[0]["latitude"])
+        return np.ndarray([(p["latitude"], p["longitude"]) for p in self.points])
 
     def is_empty(self):
         return len(self) == 0
@@ -87,15 +95,17 @@ class Cell:
             raise TopologicalError(e)
         self.polygons = [difference_shape.buffer(0)]
 
-        s_tuple = {(p.x, p.y) for p in self.points}
-        o_tuple = {(p.x, p.y) for p in other.points}
+        s_tuple = {(p["latitude"], p["longitude"]) for p in self.points}
+        o_tuple = {(p["latitude"], p["longitude"]) for p in other.points}
 
         diff_tups = s_tuple - o_tuple
 
-        self.points = [p for p in self.points if (p.x, p.y) in diff_tups]
+        self.points = [
+            p for p in self.points if (p["latitude"], p["longitude"]) in diff_tups
+        ]
 
     def separate_points(self, points, polygons, contain_points):
-        coords = ((p.x, p.y) for p in points)
+        coords = ((p["latitude"], p["longitude"]) for p in points)
         new_id = self.name + str(hash(coords)[:12])
 
         new_cell = Cell(new_id, points, polygons, self.country, self.admin1)
@@ -107,7 +117,7 @@ class Cell:
         try:
             return self.current_shape.contains(point)
         except TypeError:
-            return self.current_shape.contains(Point(point.lng, point.lat))
+            return self.current_shape.contains(Point(point[0], point[1]))
         except Exception as e:
             logger.warning(e)
 
@@ -168,7 +178,7 @@ class Cell:
         if len(cc.index) == 2:
             return self.separate_single_cluster(df, cluster=cc.iloc[0]["cluster"])
         else:
-            polygons = self.voronoi_polygons(coords=cc[["lng", "lat"]].valeus)
+            polygons = self.voronoi_polygons(coords=cc[["lng", "lat"]].values)
 
             new_cells = []
             for cluster, polygon in zip(cc["cluster"].unique(), polygons):
@@ -182,11 +192,117 @@ class Cell:
                 new_cells.append(new_cell)
             return new_cells, [self]
 
+    def split_cell(self, add_to, cluster_args, min_cell_size, max_cell_size):
+        if len(self.points) < max_cell_size:
+            return []
+
+        print(self.curr_coords)
+        df = pd.DataFrame(data=[self.curr_coords], columns=["lng", "lat"])
+        df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lng, df.lat), crs=CRS)
+
+        clusterer = OPTICS(min_samples=cluster_args[0], xi=cluster_args[1])
+        df["cluster"] = clusterer.fit_predict(df[["lng", "lat"]].values)
+
+        uniques_clusters = df["cluster"].nunique()
+        if uniques_clusters < 2:
+            return []
+
+        cluster_count = df["cluster"].value_counts()
+        large_clusters = cluster_count[cluster_count >= min_cell_size].index
+        non_null_large_clusters = [x for x in large_clusters if x != -1]
+
+        if len(large_clusters) < 2:
+            return []
+
+        if len(large_clusters) == 2 and len(non_null_large_clusters) == 1:
+            null_df = df[df["cluster"] == -1]
+            if len(null_df) > max_cell_size:
+                return []
+
+            new_cells, remove_cells = self.separate_single_cluster(
+                df, non_null_large_clusters
+            )
+        else:
+            new_cells, remove_cells = self.separate_multi_cluster(
+                df, non_null_large_clusters
+            )
+
+        for new_cell in new_cells:
+            self.subtract(new_cell)
+            add_to.add(new_cell)
+
+        for cell in remove_cells:
+            add_to.remove(cell)
+
+        clean_cells = new_cells
+        if len(remove_cells) == 0:
+            clean_cells += self
+
+        self.clean_dirty_splits(clean_cells)
+
+        proc_cells = []
+        if len(self.points) > max_cell_size and self not in remove_cells:
+            proc_cells.append(self)
+
+        for cell in new_cells:
+            if len(cell.points) > max_cell_size:
+                proc_cells.append(cell)
+
+        return proc_cells
+
+    def clean_dirty_splits(self, cells):
+        df = pd.DataFrame(data=[x.tolist() for x in cells], columns=GEOCELL_COLUMNS)
+        df = gpd.GeoDataFrame(df, geometry="geometry", crs=CRS)
+
+        multi_polys = df[df["geometry"].type == "MultiPolygon"]
+
+        for index, row in multi_polys.iterrows():
+            points = cells[index].to_pandas()["geometry"]
+
+            all_polygons = list(row["geometry"].geoms)
+
+            largest_poly = max(all_polygons, key=lambda polygon: polygon.area)
+
+            did_assign = False
+            for small_poly in all_polygons:
+                if small_poly != largest_poly:
+                    small_poly_gseries = gpd.GeoSeries(
+                        [small_poly], index=[index], crs=CRS
+                    )
+
+                    other_polys = df.drop(index)
+                    buffered_poly = small_poly_gseries.buffer(0.01)
+                    intersecting_polys = other_polys[
+                        other_polys.intersects(buffered_poly.unary_union)
+                    ]
+
+                    if len(intersecting_polys) == 0:
+                        continue
+
+                    did_assign = True
+
+                    largest_intersect_index = intersecting_polys.geometry.apply(
+                        lambda poly: poly.intersection(buffered_poly.unary_union).area
+                    ).idxmax()
+
+                    mask = points.within(small_poly)
+                    points_in_small_poly = points[mask]
+                    cells[index].points = [
+                        x for x in cells[index].points if x not in points_in_small_poly
+                    ]
+                    cells[largest_intersect_index].add_points(points_in_small_poly)
+
+                    cells[largest_intersect_index].polygons = [
+                        cells[largest_intersect_index].current_shape.union(small_poly)
+                    ]
+                if did_assign:
+                    cells[index].polygons = [largest_poly]
+
     def cluster(self):
         pass
 
     def to_pandas(self):
-        data = [[self.id, p.x, p.y] for p in self.points]
+        data = [[self.id, p["latitude"], p["longitude"]] for p in self.points]
         columns = ["id", "lng", "lat"]
         df = pd.DataFrame(data=data, columns=columns)
         df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.lng, df.lat), crs=CRS)
