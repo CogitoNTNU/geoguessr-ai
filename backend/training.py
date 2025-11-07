29d2d8c76270398945ff0789461174fd212e20ef
Jens Martin Norheim Berget <jmnb1@outlook.com>
2025-11-08 00:12:49 +0100

    fix: more decriptive variable names in training.py

--- backend/training.py
+++ backend/training.py
@@ -5,20 +5,19 @@ from data import ImageDataset
 def train_model(data, labels, num_epochs: int):
     """
     Train the model
-    Not finished
+    Data = bilder
+    Labels = tilhørende metadata med følgende felter:
+    lat: float
+    lon: float
+    heading: int
+    location_id: str
+    pano_id: str
+    capture_date: str
+    batch_date: str
     """
     dataset = ImageDataset(data, labels)
-    # Data = bilder
-    # Labels = tilhørende metadata med følgende felter:
-    # lat: float
-    # lon: float
-    # heading: int
-    # location_id: str
-    # pano_id: str
-    # capture_date: str
-    # batch_date: str
     train_dataloader = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=4)
 
     for epoch in range(num_epochs):
-        for batch_features, batch_labels in train_dataloader:
+        for batch_images, batch_metadata in train_dataloader:
             pass
