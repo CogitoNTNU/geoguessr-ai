1a08720a684c1d3782c0128554e8c805413676c1
Jens Martin Norheim Berget <jmnb1@outlook.com>
2025-11-08 00:10:40 +0100

    feat: added descriptive comment in training.py

--- backend/training.py
+++ backend/training.py
@@ -8,6 +8,15 @@ def train_model(data, labels, num_epochs: int):
     Not finished
     """
     dataset = ImageDataset(data, labels)
+    # Data = bilder
+    # Labels = tilhørende metadata med følgende felter:
+    # lat: float
+    # lon: float
+    # heading: int
+    # location_id: str
+    # pano_id: str
+    # capture_date: str
+    # batch_date: str
     train_dataloader = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=4)
 
     for epoch in range(num_epochs):
