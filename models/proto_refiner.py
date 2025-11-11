c00d1482192ebbb84041d2a507cd4e52e2589d2d
Daniel Neukirch Hansen <danielnh@stud.ntnu.no>
2025-11-11 20:22:34 +0100

    feat: adding and refactoring refiner and sql
    co-author: parleen

--- models/proto_refiner.py
+++ models/proto_refiner.py
@@ -8,12 +8,12 @@ from typing import Dict, List, Tuple
 from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from datasets import (
-    DatasetDict,
     Dataset,
     enable_progress_bar,
     disable_progress_bar,
 )
 from preprocessing.geo_utils import haversine
+from data.sql.sql_dataset_adapter import DualSQLiteEmbeddingDataset
 
 # Cluster refinement model
 
@@ -30,9 +30,10 @@ class ProtoRefiner(nn.Module):
         max_refinement: int = 1000,
         temperature: float = 1.6,
         proto_path: str = PROTO_PATH,
-        dataset_path: str = DATASET_PATH,
         protos: List[Dataset] = None,
         verbose: bool = False,
+        clip_db_path: str = "data/sqlite/clip/dataset.sqlite",  # correct?
+        tinyvit_db_path: str = "data/sqlite/tinyvit/dataset.sqlite",  # correct?
     ):
         """Proto-Net refinement model
 
@@ -59,8 +60,11 @@ class ProtoRefiner(nn.Module):
         self.max_refinement = max_refinement
         self.verbose = verbose
 
-        # Load dataset with embeddings and prototypes
-        self.dataset = DatasetDict.load_from_disk(dataset_path)
+        # TODO: get the needed data from the dataset through sql querying
+        # Check all the places where self.dataset is used
+        self.dataset = {
+            "train": DualSQLiteEmbeddingDataset(clip_db_path, tinyvit_db_path)
+        }
 
         # Load prototypes
         self.proto_df = pd.read_csv(proto_path)
@@ -68,9 +72,10 @@ class ProtoRefiner(nn.Module):
         # Load prototype index dataframe
         self.proto_df["geocell_id"] = self.proto_df["geocell_id"].astype(int)
         self.num_geocells = self.proto_df["geocell_id"].max() + 1
-        self.proto_df = self.proto_df.set_index("geocell_id")
+        # self.proto_df = self.proto_df.set_index("geocell_id") The geocell_id is not unique and the file contains all the clusters for every geocell
 
         # Generate prototypes for every geocells
+        # TODO: See what we need to load here
         if protos is None:
             self.load_prototypes()
         else:
@@ -111,17 +116,15 @@ class ProtoRefiner(nn.Module):
     def forward(
         self,
         embedding: Tensor = None,
-        geo_tensor: Tensor = None, #not used
         initial_preds: Tensor = None,
         candidate_cells: Tensor = None,
         candidate_probs: Tensor = None,
-        cluster: Tensor = None, #not used
+        cluster: Tensor = None,  # not used
     ):
         """Forward function for proto refinement model.
 
         Args:
             embedding (Tensor): CLIP embeddings of images.
-            geo_tensor (Tensor): tensor containing multi-task regression predictions.
             initial_preds (Tensor): initial predictions.
             candidate_cells (Tensor): tensor of candidate geocell predictions.
             candidate_probs (Tensor): tensor of probabilities assigned to
@@ -161,10 +164,14 @@ class ProtoRefiner(nn.Module):
                 if cell_id in [121, 650, 1859]:  # TODO: fix
                     cell_id = 1436
 
-                cell_emb = self.protos[cell.item()]
+                cell_emb = self.protos[
+                    cell_id
+                ]  # TODO:cell_emb: Tensor of shape (num_protos, dim_embedding), or None. dim_embedding:emb of pictures in that geocell
+                # might change this to:
+                # cell_emb = self.protos[cell_id]["embedding"].to("cuda")
                 if cell_emb is None:
                     if self.verbose:
-                        print(f"Proto dataset for geocell {cell.item()} is None.")
+                        print(f"Proto dataset for geocell {cell_id} is None.")
 
                     top_distances.append(torch.tensor(-100000, device="cuda"))
                     top_preds.append([0.0, 0.0])
@@ -175,8 +182,8 @@ class ProtoRefiner(nn.Module):
 
                 # Get top cluster and corresponding coordinates
                 top_distances.append(torch.max(logits).item())
-                pred_id = torch.argmax(logits, dim=-1)
-                entry = self.protos[cell.item()][pred_id.item()]
+                pred_cluster_id = torch.argmax(logits, dim=-1)
+                entry = self.protos[cell_id][pred_cluster_id.item()]
                 lng, lat = self._within_cluster_refinement(emb, entry)
                 top_preds.append([lng, lat])
 
@@ -198,7 +205,7 @@ class ProtoRefiner(nn.Module):
             distance = haversine(initial_LLH, refined_LLH)[0]
             if distance > self.max_refinement:
                 final_probs = c_probs[: self.topk]
-                if self.verbose:  
+                if self.verbose:
                     print("\t\tCancelled refinement: distance too far.")
 
             final_pred_id = torch.argmax(final_probs).item()
@@ -231,8 +238,11 @@ class ProtoRefiner(nn.Module):
             return cluster["lng"].item(), cluster["lat"].item()
 
         points = self.dataset["train"][cluster["indices"]]
-        embeddings = points["embedding"].to("cuda")
-        if embeddings.size(1) == 4:
+        embeddings = points["emb_clip"].to("cuda")  # or points["emb_tiny_vit"]
+        # if embeddings.dim() == 3 and embeddings.size(1) == 4:
+        if (
+            embeddings.dim() == 3 and embeddings.size(1) == 4
+        ):  # TODO:check this later with data
             embeddings = embeddings.mean(dim=1)
 
         distances = self._euclidean_distance(embeddings, emb)
@@ -286,6 +296,7 @@ class ProtoRefiner(nn.Module):
             Dataset: dataset including prototypes
         """
         try:
+            # TODO: Needs changing into getting the into for one cell, and not eveything in that rows
             cell_df = self.proto_df.loc[cell]
 
         # Some geocells might overlap with others, causing no data points to be in the cell
@@ -299,9 +310,10 @@ class ProtoRefiner(nn.Module):
             return None
 
         data = Dataset.from_pandas(cell_df)
+        # Data is a list of all the indices in that geocell
         data = data.map(self._compute_protos_for_cell)
         data.set_format("torch")
-        return data
+        return data  # This becomes
 
     def _cosine_similarity(self, matrix: Tensor, vector: Tensor) -> Tensor:
         """Computes the cosine similarity between all vectors in matrix and vector.
@@ -347,7 +359,7 @@ class ProtoRefiner(nn.Module):
         sum = torch.sum(ex, axis=0)
         return ex / sum
 
-    def _compute_protos_for_cell(self, example: Dict, geo: bool = False) -> Dict:
+    def _compute_protos_for_cell(self, example: Dict) -> Dict:
         """Computes embedding and geo prototypes.
 
         Args:
@@ -360,13 +372,12 @@ class ProtoRefiner(nn.Module):
         """
         indices = example["indices"]
         entries = self.dataset["train"][indices]
-        embeddings = entries["embedding"]
+        # TODO: Get the embeddings for the indices in that geocell through sql querying
+        embeddings = entries["emb_clip"]  # or "emb_tiny_vit"
         if embeddings.dim() == 3:
             embeddings = embeddings.mean(dim=1)
-
         proto_emb = embeddings.mean(dim=0)
-        if geo is False:
-            return {"embedding": proto_emb}
+        # If you also need mean geos:
+        # lng/lat come from entries["labels"] (lon, lat)
 
-        proto_geo = entries["labels_multi_task"].mean(dim=0)
-        return {"embedding": proto_emb, "geo_tensor": proto_geo}
+        return {"embedding": proto_emb}
