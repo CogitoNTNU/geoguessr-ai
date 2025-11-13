d8409141e8911d7c7c67f866c64921c8bff97fa0
Jens Martin Norheim Berget <jmnb1@outlook.com>
2025-11-13 18:36:24 +0100

    fix: Update embedding model from TINYVIT to CLIP in main_coordinator_idun.py

--- main_coordinator_idun.py
+++ main_coordinator_idun.py
@@ -126,7 +126,7 @@ def main(config):
     geocell_manager = GeocellManager("data/geocells/finished_geocells")
     num_geocells = geocell_manager.get_num_geocells()
 
-    embeddingModelUsed = "TINYVIT"  # Possible values are "CLIP" or "TINYVIT"
+    embeddingModelUsed = "CLIP"  # Possible values are "CLIP" or "TINYVIT"
 
     embedding_model = 0
     if embeddingModelUsed == "CLIP":
