3232bcc58fb83be847576e17f086d9047d18f21a
Jens Martin Norheim Berget <jmnb1@outlook.com>
2025-11-13 19:09:53 +0100

    fix: change training script to use tinyvit

--- main_coordinator_idun.py
+++ main_coordinator_idun.py
@@ -126,7 +126,7 @@ def main(config):
     geocell_manager = GeocellManager("data/geocells/finished_geocells")
     num_geocells = geocell_manager.get_num_geocells()
 
-    embeddingModelUsed = "CLIP"  # Possible values are "CLIP" or "TINYVIT"
+    embeddingModelUsed = "TINYIT"  # Possible values are "CLIP" or "TINYVIT"
 
     embedding_model = 0
     if embeddingModelUsed == "CLIP":
